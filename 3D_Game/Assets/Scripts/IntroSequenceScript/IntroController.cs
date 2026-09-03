35665d2fbd0fd441ea336d9c77148d345c52b85d
angelamotsomane3 <angelamotsomane3@gmail.com>
2026-09-03 15:44:23 +0200

    Intro sequence script

---
+++ 3D_Game/Assets/Scripts/IntroSequenceScript/IntroController.cs
@@ -0,0 +1,93 @@
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class IntroController : MonoBehaviour
+{
+    public Image storyPage;
+    public Sprite[] pages;
+
+    public CanvasGroup fadePanel;
+    public GameObject clickHint;
+
+    public AudioSource audioSource;
+    public AudioClip closetSound;
+
+    private int currentPage = 0;
+    private bool isTransitioning = false;
+
+    void Start()
+    {
+        currentPage = 0;
+        ShowPage();
+
+        clickHint.SetActive(true);
+        fadePanel.alpha = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !isTransitioning)
+        {
+            HandleClick();
+        }
+    }
+
+    void HandleClick()
+    {
+        // If you're not on the last page go to next one
+        if (currentPage < pages.Length - 1)
+        {
+            currentPage++;
+            ShowPage();
+            return;
+        }
+
+        // If YOU'RE ON THE LAST PAGE start ending sequence
+        StartCoroutine(EndSequence());
+    }
+
+    void ShowPage()
+    {
+        storyPage.sprite = pages[currentPage];
+    }
+
+    IEnumerator EndSequence()
+    {
+        isTransitioning = true;
+        clickHint.SetActive(false);
+
+        // Small tension pause before sound
+        yield return new WaitForSeconds(0.5f);
+
+        // Closet sound moment
+        if (audioSource != null && closetSound != null)
+        {
+            audioSource.PlayOneShot(closetSound);
+        }
+
+        // Wait for sound impact moment
+        yield return new WaitForSeconds(1f);
+
+        // Slow fade
+        yield return StartCoroutine(FadeOut());
+
+        //SceneManager.LoadScene("Bedroom");
+    }
+    IEnumerator FadeOut()
+    {
+        float duration = 3f; // SLOW fade 
+        float t = 0f;
+
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            fadePanel.alpha = Mathf.Lerp(0, 1, t / duration);
+            yield return null;
+        }
+
+        fadePanel.alpha = 1;
+    }
+}
